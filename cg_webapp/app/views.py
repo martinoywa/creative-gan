99f82509b3bdd4ebb2ccc4b0d31c3946a81c1b57
Martin Oywa <martinoywa2@gmail.com>
2020-03-03 10:22:35 +0300

    link static files

--- cg_webapp/app/views.py
+++ cg_webapp/app/views.py
@@ -7,3 +7,13 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('index.html')
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@main.route('/help')
+def help():
+    return render_template('help.html')
