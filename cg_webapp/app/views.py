6dc64af4a8bc2390e5a7f54745d323000cd0d1cb
Martin Oywa <martinoywa2@gmail.com>
2020-03-03 11:19:49 +0300

    generate z

--- cg_webapp/app/views.py
+++ cg_webapp/app/views.py
@@ -1,12 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import torch
 
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        latent_vector = torch.randn(1, 100, 1, 1)
+        print(latent_vector)
+
+        return render_template('index.html', latent_vector=latent_vector)
 
 
 @main.route('/about')
